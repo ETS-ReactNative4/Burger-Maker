7fb6f1c849e42887da950e342613f7b49bfb66d4
KARROTHU AKHIL <karrothuakhil@KARROTHUs-MacBook-Pro.local>
2020-03-07 19:44:20 -0500

    Adding pricing

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,18 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 
 const  burger = (props) => {
 
-    const transformedingredients = Object.keys(props.ingredients)
+    let transformedingredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
-        });
-
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedingredients.length === 0){
+        transformedingredients = <p>Please Start Building your Burger!!</p>;
+    }
   return(
     <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
